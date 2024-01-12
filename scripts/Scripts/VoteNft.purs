c41750e33f12fcc295237544a153de2d1d012f4a
nini-faroux <nfarrel1@tcd.ie>
2024-01-12 14:20:21 +0000

    Oh wrong script

--- scripts/Scripts/VoteNft.purs
+++ scripts/Scripts/VoteNft.purs
@@ -2,10 +2,11 @@ module Scripts.VoteNft
   ( voteNftPolicy
   ) where
 
-import Contract.Prelude (pure, ($))
 import Contract.Monad (Contract)
+import Contract.Prelude (pure, ($))
 import Contract.Scripts (MintingPolicy(PlutusMintingPolicy))
 import Scripts.Utils (mkScript)
 
 voteNftPolicy :: Contract MintingPolicy
-voteNftPolicy = pure $ PlutusMintingPolicy $ mkScript "./scripts/Json/Optimised/VoteNft.json"
+voteNftPolicy = pure $ PlutusMintingPolicy $ mkScript
+  "./scripts/Json/Optimised/AlwaysMints.json"
